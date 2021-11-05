a6118bc677658106bf280f040bb59a0e15ce5ccf
catstacks <61471777+catstacks@users.noreply.github.com>
2021-11-05 09:21:39 +0000

    Update tkinter_practice.py

--- tkinter_practice.py
+++ tkinter_practice.py
@@ -2,14 +2,16 @@ from tkinter import *
 
 root = Tk() # This line must come before anything else; ALWAYS the first step. 
 
-# In Tkinter you must (1) define and create something (2) put it up on the screen.    
+# In Tkinter you must (1) define and create something (2) put it up on the screen.
+e = Entry(root) # This can be used to create a simple input field
+e.pack()    # See above comment
 # Initial Button test below:
 
 def myClick():
-    myLabel = Label(root, text="The button click worked!!")
+    myLabel = Label(root, text=e.get()) # Allows user to write in the input field, returns what they wrote
     myLabel.pack()
 
-myButton = Button(root, text = "Click here!", command=myClick)
+myButton = Button(root, text = "Say Something Cool", command=myClick)
 myButton.pack()
 
 root.mainloop()
